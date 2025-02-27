a6b4390da536b4584062eef4aa3cf151b97c5aef
Mikhail Toropchinov <ruhose73@gmail.com>
2025-02-27 23:01:29 +0300

    feat(boiler): add configs and libs

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-empty": [2, "never"],
+    "subject-empty": [2, "never"], 
+    "header-max-length": [2, "always", 72],
+  },
+};
